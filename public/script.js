63eeeb133a9b5d7fe880b8592e59599cad5aa3cc
Gladimore <105724638+Gladimore@users.noreply.github.com>
2024-06-26 01:43:35 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,9 @@
 const creditsContainer = document.querySelector('.credits');
 
+fetch("/word")
+.then(response => response.text())
+.then((data) => document.querySelector(".word").innerText = data)
+
 document.addEventListener('scroll', () => {
     const scrollTop = window.scrollY;
     const scrollBottom = scrollTop + window.innerHeight;
